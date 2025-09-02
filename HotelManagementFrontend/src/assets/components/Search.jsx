8b198546ea0f35e2d4aa2b599da196f2005afe29
Priydev Mishra <priydevmishra389@gmail.com>
2025-09-02 15:10:07 +0530

    Change button text from 'Search' to 'Searching'

--- HotelManagementFrontend/src/assets/components/Search.jsx
+++ HotelManagementFrontend/src/assets/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = () => {
   return (
     <form className='mt-7 flex justify-center'>
       <input className='border-[#ccc] border-[1px] rounded p-1.5 w-xl text-center' type="text" placeholder='Search Here'/>
-      <button className='border-[1px] border-green-500 p-1.5 rounded ml-1 text-green-500 bg-white hover:bg-green-500 hover:text-black cursor-pointer'>Search</button>
+      <button className='border-[1px] border-green-500 p-1.5 rounded ml-1 text-green-500 bg-white hover:bg-green-500 hover:text-black cursor-pointer'>Searching</button>
     </form>
   )
 }
